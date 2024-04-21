816ee9df9db7748fc6aee9064501be98f52ceee1
misha <myhajlo.konovalov@gmail.com>
2024-04-21 16:35:51 +0300

    BS-14: Fix layouts

--- assets/controllers/carousel_controller.js
+++ assets/controllers/carousel_controller.js
@@ -3,12 +3,10 @@ import Carousel from '@stimulus-components/carousel'
 export default class CarouselController extends Carousel {
     get defaultOptions() {
         return {
-            spaceBetween: 20,
-            slidesPerView: 3,
+            spaceBetween: 10,
+            slidesPerView: 4,
+            autoHeight: true,
             loop: true,
-            pagination: {
-                el: '.swiper-pagination',
-            },
             navigation: {
                 prevEl: '.swiper-button-prev',
                 nextEl: '.swiper-button-next',
